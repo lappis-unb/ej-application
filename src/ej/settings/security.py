63725ce6c40291c9b0d679a0421c497dc8cbb925
David Carlos <ddavidcarlos1392@gmail.com>
2024-01-29 20:22:06 -0300

    Removes unnecessary code from security.py

--- src/ej/settings/security.py
+++ src/ej/settings/security.py
@@ -21,24 +21,12 @@ class SecurityConf(Base):
     HTTP_ACCESS_CONTROL_ALLOW_CREDENTIALS = env("", name="{attr}")
     HTTP_X_FRAME_OPTIONS = env("", name="{attr}")
 
-    def get_cors_origin_whitelist(self, hostname):
-        return self.CSRF_TRUSTED_ORIGINS
-
-    def get_csrf_trusted_origins(self, hostname):
-        trusted = [hostname, *(self.env("DJANGO_CSRF_TRUSTED_ORIGINS", type=list) or ())]
-        return unique(trusted)
-
-    def get_allowed_hosts(self, hostname):
-        allowed = self.env.list("DJANGO_ALLOWED_HOSTS", default=[]) or []
-        return unique([hostname, *allowed])
-
     def finalize(self, settings):
         settings = super().finalize(settings)
 
         if self.ENVIRONMENT == "local":
             settings["INTERNAL_IPS"].append("127.0.0.1")
             settings["CORS_ORIGIN_ALLOW_ALL"] = True
-            settings["CSRF_TRUSTED_ORIGINS"] = []
         return settings
 
 
