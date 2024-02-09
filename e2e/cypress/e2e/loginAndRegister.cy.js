0e5954e177435a77d81b765e03a2211dc0f690a7
David Carlos <ddavidcarlos1392@gmail.com>
2024-02-09 17:49:19 +0000

    Adds e2e tests with Cypress

---
+++ e2e/cypress/e2e/loginAndRegister.cy.js
@@ -0,0 +1,13 @@
+describe('The login page', () => {
+  it('trigger error for unexistent user', () => {
+    cy.removesCypressUser()
+    cy.login()
+    cy.get(".errorlist li").should('contain', 'E-mail ou senha inválida')
+  })
+
+  it('Check our welcome for new user', () => {
+    cy.removesCypressUser()
+    cy.registerUser()
+    cy.get('.welcome h1').should('contain', 'Bem vindo à EJ')
+  })
+})
