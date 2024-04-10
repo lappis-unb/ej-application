cab8ae8a903864b4db663ac9a7ff012942de82b0
David Carlos <ddavidcarlos1392@gmail.com>
2024-04-10 21:55:22 +0000

    Revert "Merge branch 'logo-upload-1167' into 'develop'"

--- src/ej_tools/tests/conftest.py
+++ src/ej_tools/tests/conftest.py
@@ -1,8 +1,14 @@
 import pytest
 from django.conf import settings
+from django.core.files import File
 
 from ej_conversations.tests.conftest import *
 from ej_tools.models import OpinionComponent
+
+SOME_FILE = (
+    settings.BASE_DIR
+    / "src/ej/static/ej/assets/img/tools/opinion-component-default-background.jpg"
+)
 
 
 @pytest.fixture
@@ -10,5 +16,7 @@ def opinion_component(db, conversation):
     instance = OpinionComponent()
     instance.conversation = conversation
     instance.final_voting_message = "texto final de votação"
+    with open(str(SOME_FILE), "rb") as _file:
+        instance.logo_image.save("logo.jpg", File(_file))
     instance.save()
     return instance
