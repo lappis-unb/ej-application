8585ea7ca6fedfd52dfef4b46d9edb8762be250c
David Carlos <ddavidcarlos1392@gmail.com>
2024-05-15 19:35:57 +0000

    Fixes scatter visualization

--- src/ej_dataviz/utils.py
+++ src/ej_dataviz/utils.py
@@ -254,7 +254,7 @@ def create_stereotype_coords(
             "name", index="users"
         )
         if labels.shape != (0, 0):
-            table["cluster"] = labels.loc[labels.index.values is not None]
+            table["cluster"] = labels.loc[labels.index.values != None]  # noqa: E711
             table["cluster"].fillna(__("*Unknown*"), inplace=True)
             kwargs["labels"] = labels
 
