acdb1a3a98b7198fe15ff7b1812b106ad6de928c
Giovanni Giampauli <giovanni.acg@gmail.com>
2024-07-09 10:53:59 -0300

    test: creating a new tests

--- src/ej_users/tests/test_api.py
+++ src/ej_users/tests/test_api.py
@@ -1,6 +1,103 @@
 from ej_users.models import User
 from ej_conversations.tests.conftest import API_V1_URL
 from rest_framework.test import APIClient
+from enum import Enum
+import json
+
+
+class UserType(Enum):
+    ANONYMOUS = "anonymous"
+    AUTH = "auth"
+
+
+class UserFake:
+    USERS = {
+        UserType.ANONYMOUS: {
+            "name": "anonymous user",
+            "email": "anonymous@mail.com",
+            "password": "pass123",
+            "password_confirm": "pass123",
+        },
+        UserType.AUTH: {
+            "name": "Giovanni Giampauli",
+            "email": "giovanni@mail.com",
+            "password": "pass123@123",
+            "password_confirm": "pass123@123",
+        },
+    }
+
+
+class EJRequests:
+    def __init__(self, client):
+        self.client = client
+
+    def get_token(self, user_type: UserType, secret_id=None):
+        user = UserFake.USERS[user_type]
+        data = {
+            "email": user["email"],
+            "password": user["password"],
+        }
+
+        if secret_id:
+            data["secret_id"] = secret_id
+
+        response = self.client.post(
+            API_V1_URL + "/login/",
+            data=data,
+            content_type="application/json",
+        )
+        response_data = response.json()
+        return response_data["access_token"]
+
+    def create_user(self, user_type: UserType, secret_id=None):
+        user = UserFake.USERS[user_type]
+        data = user
+
+        if secret_id:
+            data["secret_id"] = secret_id
+
+        response = self.client.post(
+            API_V1_URL + "/users/",
+            data=data,
+            content_type="application/json",
+        )
+
+        return response
+
+    def get_user(self, user_type: UserType, token):
+        user = UserFake.USERS[user_type]
+        email = user["email"]
+        response = self.client.get(
+            API_V1_URL + f"/users/{email}/",
+            HTTP_AUTHORIZATION=f"Bearer {token}",
+        )
+        return response
+
+    def update_user(self, user_type: UserType, secret_id, token):
+        user = UserFake.USERS[user_type]
+        data = {"email": user["email"]}
+
+        response = self.client.put(
+            API_V1_URL + f"/users/{secret_id}/",
+            data=data,
+            content_type="application/json",
+            HTTP_AUTHORIZATION=f"Bearer {token}",
+        )
+        return response
+
+    def delete_user(self, email, token):
+        response = self.client.delete(
+            API_V1_URL + f"/users/{email}/",
+            HTTP_AUTHORIZATION=f"Bearer {token}",
+        )
+        return response
+
+    def get_users(self, token):
+        response = self.client.get(
+            API_V1_URL + "/users/",
+            HTTP_AUTHORIZATION=f"Bearer {token}",
+        )
+        return response
 
 
 class TestUserAPI:
@@ -98,7 +195,7 @@ class TestUserAPI:
             content_type="application/json",
         )
         assert response.json()["access_token"]
-        assert response.status_code == 200
+        assert response.status_code == 201
 
     def test_missing_credentials_in_login_should_return_error(self, client, db):
         User.objects.create_user("user@user.com", "password")
@@ -137,173 +234,70 @@ class TestUserAPI:
         assert response.status_code == 200
 
     def test_create_user_with_secret_id(self, client, db):
-        NAME = "Giovanni Giampauli"
-        EMAIL = "giovanni@example.com"
-        PASSWORD = "pass123"
-        SECRET_ID = "123456"
-        client.post(
-            API_V1_URL + "/users/",
-            data={
-                "name": NAME,
-                "email": EMAIL,
-                "password": PASSWORD,
-                "password_confirm": PASSWORD,
-                "secret_id": SECRET_ID,
-            },
-            content_type="application/json",
-        )
-        user = User.objects.get(email=EMAIL)
-        assert user.name == NAME
-        assert user.email == EMAIL
+        ej_requests = EJRequests(client)
+        SECRET_ID = "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92"
+
+        response = ej_requests.create_user(UserType.AUTH, secret_id=SECRET_ID)
+        assert response.status_code == 201
+
+        user = User.objects.get(email=UserFake.USERS[UserType.AUTH]["email"])
+        assert user.name == UserFake.USERS[UserType.AUTH]["name"]
+        assert user.email == UserFake.USERS[UserType.AUTH]["email"]
         assert user.secret_id == SECRET_ID
 
     def test_create_auth_user(self, client, db):
-        NAME_ANONYMOUS = "anonymous user"
-        NAME_AUTH = "Giovanni Giampauli"
-        EMAIL_AUTH = "giovanni@example.com"
-        EMAIL_ANONYMOUS = "anonymous@example.com"
-        PASSWORD = "pass123"
-        SECRET_ID = "123456"
+        ej_requests = EJRequests(client)
+        SECRET_ID = "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92"
 
-        response = client.post(
-            API_V1_URL + "/users/",
-            data={
-                "name": NAME_ANONYMOUS,
-                "email": EMAIL_ANONYMOUS,
-                "password": PASSWORD,
-                "password_confirm": PASSWORD,
-                "secret_id": SECRET_ID,
-            },
-            content_type="application/json",
-        )
-
+        # Create anonymous user
+        response = ej_requests.create_user(UserType.ANONYMOUS, secret_id=SECRET_ID)
         assert response.status_code == 201
 
-        client.post(
-            API_V1_URL + "/users/",
-            data={
-                "name": NAME_AUTH,
-                "email": EMAIL_AUTH,
-                "password": PASSWORD,
-                "password_confirm": PASSWORD,
-                "secret_id": SECRET_ID,
-            },
-            content_type="application/json",
-        )
-
+        # Create auth user
+        response = ej_requests.create_user(UserType.AUTH)
         assert response.status_code == 201
 
-        user = User.objects.get(email=EMAIL_AUTH)
-        assert user.name == NAME_AUTH
-        assert user.email == EMAIL_AUTH
-        assert user.secret_id == SECRET_ID
+        access_token = ej_requests.get_token(UserType.AUTH)
+
+        # Link secret_id to auth user
+        response = ej_requests.update_user(
+            SECRET_ID, access_token, {"email": UserFake.USERS[UserType.AUTH]["email"]}
+        )
+        assert response.status_code == 200
 
         user = User.objects.get(secret_id=SECRET_ID)
-        assert user.name == NAME_AUTH
-        assert user.email == EMAIL_AUTH
+        assert user.name == UserFake.USERS[UserType.AUTH]["name"]
+        assert user.email == UserFake.USERS[UserType.AUTH]["email"]
+        assert user.secret_id == SECRET_ID
+
+        user = User.objects.get(email=UserFake.USERS[UserType.AUTH]["email"])
+        assert user.name == UserFake.USERS[UserType.AUTH]["name"]
+        assert user.email == UserFake.USERS[UserType.AUTH]["email"]
         assert user.secret_id == SECRET_ID
 
         user = None
         try:
-            user = User.objects.get(email=EMAIL_ANONYMOUS)
-        except User.DoesNotExist:
-            pass
-        assert user is None
-
-    def test_create_auth_user_after_both_accounts(self, client, db):
-        NAME_ANONYMOUS = "anonymous user"
-        NAME_AUTH = "Giovanni Giampauli"
-        EMAIL_AUTH = "giovanni@example.com"
-        EMAIL_ANONYMOUS = "anonymous@example.com"
-        PASSWORD = "pass123"
-        SECRET_ID = "123456"
-
-        response = client.post(
-            API_V1_URL + "/users/",
-            data={
-                "name": NAME_ANONYMOUS,
-                "email": EMAIL_ANONYMOUS,
-                "password": PASSWORD,
-                "password_confirm": PASSWORD,
-                "secret_id": SECRET_ID,
-            },
-            content_type="application/json",
-        )
-
-        assert response.status_code == 201
-
-        response = client.post(
-            API_V1_URL + "/users/",
-            data={
-                "name": NAME_AUTH,
-                "email": EMAIL_AUTH,
-                "password": PASSWORD,
-                "password_confirm": PASSWORD,
-            },
-            content_type="application/json",
-        )
-
-        assert response.status_code == 201
-
-        response = client.post(
-            API_V1_URL + "/users/",
-            data={
-                "name": NAME_AUTH,
-                "email": EMAIL_AUTH,
-                "password": PASSWORD,
-                "password_confirm": PASSWORD,
-                "secret_id": SECRET_ID,
-            },
-            content_type="application/json",
-        )
-
-        assert response.status_code == 201
-
-        user = User.objects.get(email=EMAIL_AUTH)
-        assert user.name == NAME_AUTH
-        assert user.email == EMAIL_AUTH
-        assert user.secret_id == SECRET_ID
-
-        user = User.objects.get(secret_id=SECRET_ID)
-        assert user.name == NAME_AUTH
-        assert user.email == EMAIL_AUTH
-        assert user.secret_id == SECRET_ID
-
-        user = None
-        try:
-            user = User.objects.get(email=EMAIL_ANONYMOUS)
+            user = User.objects.get(email=UserFake.USERS[UserType.ANONYMOUS]["email"])
         except User.DoesNotExist:
             pass
         assert user is None
 
     def test_get_token_by_secret_id(self, client, db):
-        NAME = "anonymous user"
-        EMAIL = "anonymous@example.com"
-        PASSWORD = "pass123"
-        SECRET_ID = "123456"
+        ej_requests = EJRequests(client)
+        SECRET_ID = "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92"
 
-        response = client.post(
-            API_V1_URL + "/users/",
-            data={
-                "name": NAME,
-                "email": EMAIL,
-                "password": PASSWORD,
-                "password_confirm": PASSWORD,
-                "secret_id": SECRET_ID,
-            },
-            content_type="application/json",
-        )
-
+        response = ej_requests.create_user(UserType.ANONYMOUS, secret_id=SECRET_ID)
         assert response.status_code == 201
 
-        response = client.post(
-            API_V1_URL + "/token/",
-            data={
-                "secret_id": SECRET_ID,
-                "password": PASSWORD,
-            },
-            content_type="application/json",
-        )
+        access_token = ej_requests.get_token(UserType.ANONYMOUS, secret_id=SECRET_ID)
+        assert access_token
 
-        assert response.status_code == 200
-        assert response.json()["access_token"]
+    def test_get_token_by_secret_id_after_link(self, client, db):
+        ej_requests = EJRequests(client)
+        SECRET_ID = "123456"
+
+        response = ej_requests.create_user(UserType.ANONYMOUS, secret_id=SECRET_ID)
+        assert response.status_code == 201
+
+        access_token = ej_requests.get_token(UserType.ANONYMOUS, secret_id=SECRET_ID)
+        assert access_token
