49e71f214e83588e8d9fbaff2fc62f82dbe1c792
Shayane <shayaneliebe@gmail.com>
2024-08-01 15:47:45 +0000

    Create board conversations api 1479

--- src/ej_boards/api.py
+++ src/ej_boards/api.py
@@ -1,10 +1,15 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 from ej.permissions import IsAuthenticatedOnlyGetView
 from .models import Board
-from .serializers import BoardSerializer
+from .serializers import BoardDetailSerializer, BoardSerializer
 
 
 class BoardViewSet(viewsets.ModelViewSet):
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
     permission_classes = [IsAuthenticatedOnlyGetView]
+
+    def retrieve(self, request, pk):
+        board = self.get_object()
+        return Response(BoardDetailSerializer(board).data)
---
+++ src/ej_boards/tests/test_api.py
@@ -0,0 +1,105 @@
+import pytest
+from django.utils.translation import gettext_lazy as _
+
+from ej_users.models import User
+from ej_conversations.mommy_recipes import ConversationRecipes
+from ej_conversations import create_conversation
+from ej_conversations.tests.conftest import (
+    API_V1_URL,
+    ApiClient,
+    get_authorized_api_client,
+)
+
+
+class TestGetBoards(ConversationRecipes):
+    AUTH_ERROR = {"detail": _("Authentication credentials were not provided.")}
+
+    @pytest.fixture
+    def user(db):
+        user = User.objects.create_user("email2@server.com", "password")
+        user.save()
+        return user
+
+    @pytest.fixture
+    def conversation_with_board(self, board, user_db):
+        board.owner = user_db
+        board.save()
+        conversation = create_conversation("foo", "conv1", user_db, board=board)
+        conversation.board = board
+        conversation.save()
+        return conversation
+
+    @pytest.mark.django_db
+    def test_boards_endpoint(self, user, board, conversation_with_board):
+        BOARD = {
+            "title": board.title,
+            "slug": board.slug,
+            "owner": board.owner.email,
+            "description": board.description,
+            "links": {"self": "http://testserver/api/v1/boards/1/"},
+        }
+
+        api = get_authorized_api_client({"email": user.email, "password": "password"})
+        path = API_V1_URL + "/boards/"
+        result = api.get(path)
+        assert result.data.get("results")[0] == BOARD
+
+    def test_boards_endpoint_not_authenticated(self, client, board):
+        BOARD = {
+            "title": board.title,
+            "slug": board.slug,
+            "owner": board.owner.email,
+            "description": board.description,
+            "links": {"self": "http://testserver/api/v1/boards/1/"},
+        }
+        path = API_V1_URL + "/boards/"
+        assert ApiClient(client).get(path, BOARD).data == self.AUTH_ERROR
+
+    @pytest.mark.django_db
+    def test_boards_endpoint_with_conversations_list(
+        self, user, board, conversation_with_board
+    ):
+        BOARD = {
+            "title": "Title",
+            "slug": "board-slug",
+            "owner": "user@domain.com",
+            "description": "Description",
+            "conversations": [
+                {
+                    "text": "foo",
+                    "statistics": {
+                        "votes": {"agree": 0, "disagree": 0, "skip": 0, "total": 0},
+                        "comments": {
+                            "approved": 0,
+                            "rejected": 0,
+                            "pending": 0,
+                            "total": 0,
+                        },
+                        "participants": {"voters": 0, "commenters": 0},
+                        "channel_votes": {
+                            "webchat": 0,
+                            "telegram": 0,
+                            "whatsapp": 0,
+                            "opinion_component": 0,
+                            "unknown": 0,
+                            "ej": 0,
+                        },
+                        "channel_participants": {
+                            "webchat": 0,
+                            "telegram": 0,
+                            "whatsapp": 0,
+                            "opinion_component": 0,
+                            "unknown": 0,
+                            "ej": 0,
+                        },
+                    },
+                    "participants_can_add_comments": True,
+                    "anonymous_votes_limit": 0,
+                }
+            ],
+        }
+
+        api = get_authorized_api_client({"email": user.email, "password": "password"})
+        path = API_V1_URL + "/boards/1/"
+        result = api.get(path)
+        assert result.data == BOARD
