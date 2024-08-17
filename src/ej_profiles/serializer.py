4544471478139830448280afbe3413d100e7097f
Giovanni Giampauli <giovanni.acg@gmail.com>
2024-08-17 19:49:39 -0300

    feat: open put api to new profile mode

--- src/ej_profiles/serializer.py
+++ src/ej_profiles/serializer.py
@@ -1,10 +1,33 @@
 from ej_profiles.models import Profile
 from rest_framework import serializers
+from .enums import Race, Gender, Region
 
 
 class ProfileSerializer(serializers.Serializer):
-    phone_number = serializers.CharField(max_length=11)
+    phone_number = serializers.CharField(max_length=20, required=False)
+    race = serializers.ChoiceField(
+        choices=[(tag.value, tag) for tag in Race], required=False
+    )
+    gender = serializers.ChoiceField(
+        choices=[(tag.value, tag) for tag in Gender], required=False
+    )
+    birth_date = serializers.DateField(required=False)
+    region = serializers.ChoiceField(
+        choices=[(tag.value, tag) for tag in Region], required=False
+    )
 
     class Meta:
         model = Profile
-        fields = ["phone_number"]
+        fields = [
+            "phone_number",
+            "race",
+            "gender",
+            "birth_date",
+            "region",
+        ]
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
