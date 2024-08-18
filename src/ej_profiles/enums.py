ebec38f96dd8e405e93c9d92ba0548062df65595
Giovanni Giampauli <giovanni.acg@gmail.com>
2024-08-18 20:14:00 -0300

    feat: add new attr in conversation to api

--- src/ej_profiles/enums.py
+++ src/ej_profiles/enums.py
@@ -3,14 +3,31 @@ from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 from environ import ImproperlyConfigured
 
+_ethnicity_enums = getattr(settings, "EJ_PROFILE_ETHNICITY_CHOICES", None)
 _race_enums = getattr(settings, "EJ_PROFILE_RACE_CHOICES", None)
 _gender_enums = getattr(settings, "EJ_PROFILE_GENDER_CHOICES", None)
 _not_filed = getattr(settings, "EJ_NOT_FILLED_MARK", "---------")
 _region_enums = getattr(settings, "EJ_PROFILE_REGION_CHOICES", None)
+_age_enums = getattr(settings, "EJ_PROFILE_AGE_CHOICES", None)
 
 # Here we fool the IntEnum metaclass into believing that items of a list are
 # methods
 _to_thunks = lambda lst: ((lambda: k, lambda: v) for k, v in lst)
+
+
+class Ethnicity(IntEnum):
+    NOT_FILLED = 0, _not_filed
+
+    if _ethnicity_enums is None:
+        INDIGENOUS = 1, _("Indigenous")
+        BLACK = 2, _("Black")
+        BROWN = 3, _("Brown")
+        WHITE = 4, _("White")
+        YELLOW = 5, _("Yellow")
+        PREFER_NOT_TO_SAY = 6, _("Prefer not to say")
+    else:
+        for _k, _v in _to_thunks(_ethnicity_enums.items()):
+            locals()[_k()] = _v()
 
 
 class Race(IntEnum):
@@ -52,6 +69,21 @@ class Gender(IntEnum):
         OTHER = 20, _("Other")
     else:
         for _k, _v in _to_thunks(_gender_enums.items()):
+            locals()[_k()] = _v()
+
+
+class AgeRange(IntEnum):
+    NOT_FILLED = 0, _not_filed
+
+    if _age_enums is None:
+        RANGE_1 = 1, _("Less than 17 years")
+        RANGE_2 = 2, _("Between 17-20 years")
+        RANGE_3 = 3, _("Between 21-29 years")
+        RANGE_4 = 4, _("Between 30-45 years")
+        RANGE_5 = 5, _("Between 45-60 years")
+        RANGE_6 = 6, _("Over 60 years")
+    else:
+        for _k, _v in _to_thunks(_age_enums.items()):
             locals()[_k()] = _v()
 
 
--- src/ej_profiles/serializer.py
+++ src/ej_profiles/serializer.py
@@ -1,11 +1,14 @@
 from ej_profiles.models import Profile
 from rest_framework import serializers
-from .enums import Race, Gender, Region
+from .enums import Ethnicity, Race, Gender, Region, AgeRange
 
 
 class ProfileSerializer(serializers.Serializer):
     user = serializers.PrimaryKeyRelatedField(read_only=True)
     phone_number = serializers.CharField(max_length=20, required=False)
+    ethnicity_choices = serializers.ChoiceField(
+        choices=[(tag.value, tag) for tag in Ethnicity], required=False
+    )
     race = serializers.ChoiceField(
         choices=[(tag.value, tag) for tag in Race], required=False
     )
@@ -13,6 +16,9 @@ class ProfileSerializer(serializers.Serializer):
         choices=[(tag.value, tag) for tag in Gender], required=False
     )
     birth_date = serializers.DateField(required=False)
+    age_range = serializers.ChoiceField(
+        choices=[(tag.value, tag) for tag in AgeRange], required=False
+    )
     region = serializers.ChoiceField(
         choices=[(tag.value, tag) for tag in Region], required=False
     )
@@ -22,9 +28,11 @@ class ProfileSerializer(serializers.Serializer):
         fields = [
             "user",
             "phone_number",
+            "ethnicity_choices",
             "race",
             "gender",
             "birth_date",
+            "age_range",
             "region",
         ]
 
